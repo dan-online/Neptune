ddb834fbc50991206d0e675ce85308c80e01646b
Dan <dan@dancodes.online>
2020-05-12 18:29:13 +0200

    v1 of config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  prefix: "t!",
+  owner: "312551747027468290", // your discord id
+  emojis: {
+    err: {
+      full: ":red_circle:",
+      id: ":red_circle:",
+    },
+    success: {
+      id: ":white_check_mark:",
+      full: ":white_check_mark:",
+    },
+  },
+  persistent: false,
+};
