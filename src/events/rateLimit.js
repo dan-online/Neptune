2ee872d09792663c54c68ab81fc7ac9006932c5a
Dan <dan@dancodes.online>
2020-05-18 10:42:43 +0200

    better ratelimit message

--- src/events/rateLimit.js
+++ src/events/rateLimit.js
@@ -1,6 +1,12 @@
 module.exports = {
   name: "rateLimit",
   event(client, info) {
-    console.log("RATELIMIT", info);
+    log("lmit")(
+      "ratelimited for " +
+        info.limit +
+        " actions and " +
+        Math.round((info.timeout / 1000) * 100) / 100 +
+        "s"
+    );
   },
 };
