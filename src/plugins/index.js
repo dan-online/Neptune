6458441819dbd1e520f3533dd62ed863da60e113
Dan <dan@dancodes.online>
2020-05-19 17:13:35 +0200

    global plugins fix in src/plugins/index.js

---
+++ src/plugins/index.js
@@ -0,0 +1,9 @@
+const utilFiles = fs.readdirSync(path.resolve(__dirname));
+utilFiles.forEach((x) => {
+  const name = x.split(".js")[0];
+  if (x == "index.js") return;
+  if (!process.conf[name] || !process.conf[name].enabled) return;
+  module.exports[name] = new (require(path.resolve(__dirname, x)))(
+    process.conf[name]
+  );
+});
