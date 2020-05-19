5b23a2105efbfd281827d43199a7428a25c016ce
Dan <dan@dancodes.online>
2020-05-19 17:13:35 +0200

    global plugins fix in src/commands/ticket.js

--- src/commands/ticket.js
+++ src/commands/ticket.js
@@ -1,14 +1,15 @@
 module.exports = {
-  plugins: ["tickets"],
   aliases: ["ticket", "t"],
   use: process.conf.prefix + "ticket <command> <reason/option>",
   desc: "Open a ticket for support",
 };
 
-module.exports.run = async (client, message, args, { plugins }) => {
-  const { tickets } = plugins;
+module.exports.run = async (client, message, args) => {
+  const { tickets } = Plugins;
   const guildTickets = tickets.fetch(message.guild.id) || [];
-  const openTicket = guildTickets.find((x) => x.user === message.author.id);
+  const openTicket = guildTickets.find(
+    (x) => x.user === message.author.id && x.status.open == true
+  );
   switch (args[0]) {
     case "open":
       if (openTicket) {
@@ -28,20 +29,34 @@ module.exports.run = async (client, message, args, { plugins }) => {
         break;
       }
     case "close":
-      if (!openTicket) {
-        if (args[1]) {
-          let ticketToClose = guildTickets.find((x) => x.number == args[1]);
-          if (!ticketToClose) throw new Error("No ticket with that ID found!");
-          if (!args.find((x) => x == "YES")) {
-            throw new Error("To continue run the command again with ``YES``");
-          } else {
-          }
+      if (args[1] && !isNaN(args[1])) {
+        if (message.member.hasPermission("ADMINISTRATOR"))
+          throw new Error("Only an admin can close other tickets!");
+        let ticketToClose = guildTickets.find((x) => x.number == args[1]);
+        if (!ticketToClose) throw new Error("No ticket with that ID found!");
+        if (!args.find((x) => x == "YES")) {
+          return message.channel.send(
+            "To continue run the command again with ``YES``"
+          );
         } else {
+          tickets.close(message.author, message.guild, args[1]);
+          message.channel.send("Ticket has been closed successfully!");
+        }
+      } else {
+        if (!openTicket)
           throw new Error(
             "You didn't specify a ticket to close and you don't have an open ticket"
           );
+        if (!args.find((x) => x == "YES")) {
+          throw new Error("To continue run the command again with ``YES``");
+        } else {
+          tickets.close(message.author, message.guild, openTicket.number);
+          message.channel.send("Ticket has been closed successfully!");
         }
       }
+      break;
+    case "list":
+      const embed = new Discord.MessageEmbed();
       break;
     default:
       throw new Error(args[0] ? "Invalid command" : "No command provided");
