b393cdc8b12109826d87a1a58300341d02ea77ef
Dan <dan@dancodes.online>
2020-05-19 10:00:22 +0200

    WIP ticket system, it works, mostly

--- src/commands/ticket.js
+++ src/commands/ticket.js
@@ -8,5 +8,8 @@ module.exports = {
 module.exports.run = async (client, message, args, { plugins }) => {
   const { tickets } = plugins;
   if (tickets.find((x) => x.user === message.author.id)) {
+    return message.channel.send("You already have an open ticket!");
   }
+  let ticket = tickets.open(message.author, message.guild, args.join(" "));
+  message.channel.send("Your ticket has been created! Head over to #" + ticket);
 };
--- src/plugins/tickets.js
+++ src/plugins/tickets.js
@@ -1,7 +1,49 @@
+const { use } = require("../commands/autorole");
+const ticket = require("../commands/ticket");
+
 module.exports = class Tickets extends Enmap {
   constructor(config) {
     super(process.conf.persistent ? { name: "tickets" } : null);
     this.config = config;
     return this;
+  }
+  open(user, guild, reason) {
+    this.tickets = this.get(guild.id) || [];
+    this.guild = guild;
+    let ticketNumber = this.tickets.length + 1;
+    guild.channels
+      .create(ticketNumber, {
+        type: "text",
+        topic: "Ticket " + ticketNumber + ": " + reason,
+        position: 0,
+        reason: "ticket made by " + user.tag,
+      })
+      .then((channel) => {
+        channel.updateOverwrite(user.id, {
+          SEND_MESSAGES: true,
+          READ_MESSAGES: true,
+          VIEW_CHANNEL: true,
+        });
+        channel.updateOverwrite(channel.guild.roles.everyone, {
+          VIEW_CHANNEL: false,
+        });
+        this.tickets.push({ number: ticketNumber, reason, date: new Date() });
+        console.log(this.tickets);
+        this.save();
+        channel.send(
+          new Discord.MessageEmbed()
+            .setTitle("Ticket #" + ticketNumber)
+            .setDescription(
+              `User: ${user}\nDescription: ${reason}\nDate: ${new Date().toLocaleString()}`
+            )
+            .setColor(process.conf.color)
+            .setTimestamp()
+        );
+      });
+    return ticketNumber;
+  }
+  save() {
+    console.log(this.tickets, this.guild.id);
+    this.set(this.guild.id, this.tickets);
   }
 };
