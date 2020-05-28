1f0cfab9d772c5458cfca718f0898584a46ede9e
LightenedLimited <macnguyenanh@gmail.com>
2020-05-28 11:47:55 +0200

    Simplyfing the methods

--- src/plugins/webhooks.js
+++ src/plugins/webhooks.js
@@ -11,30 +11,9 @@ class Webhooks {
         this.app = express();
         console.log(this.app)
         this.routes.forEach((route) => {
-            if (route.method == "GET") {
-                this.app.get(route.route, (req, res) => {
-                    require(resolve(join(__dirname, route.handler)))(req, res, this.client);
-                })
-                return;
-            }
-            if (route.method == "POST") {
-                this.app.post(route.route, (req, res) => {
-                    require(resolve(join(__dirname, route.handler)))(req, res, this.client);
-                })
-                return;
-            }
-            if (route.method == "PUT") {
-                this.app.put(route.route, (req, res) => {
-                    require(resolve(join(__dirname, route.handler)))(req, res, this.client);
-                })
-                return;
-            }
-            if (route.method == "DELETE") {
-                this.app.delete(route.route, (req, res) => {
-                    require(resolve(join(__dirname, route.handler)))(req, res, this.client);
-                })
-                return;
-            }
+            this.app[route.method](route.route, (req, res) => {
+                require(resolve(join(__dirname, route.handler)))(req, res, this.client);
+            })
         });
         this.app.listen(config.port, () => {
             console.log("App is listening on port" + config.port);
