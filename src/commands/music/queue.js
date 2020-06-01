812d507f9ce5325cd0edb36dada6cf2c5af73d6a
Dan <dan@dancodes.online>
2020-06-01 22:13:26 +0200

    fix disable and queue in src/commands/music/queue.js

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -1,16 +1,16 @@
 module.exports = {
-    aliases: ["q", "queue"],
-    use: process.conf.prefix + "queue",
-    desc: "Get current music queue",
-    // disabled: !(process.conf.music && process.conf.economy.music),
+  aliases: ["q", "queue"],
+  use: process.conf.prefix + "queue",
+  desc: "Get current music queue",
+  disabled: !(process.conf.music && process.conf.music.enabled),
 };
 
 module.exports.run = async (client, message, args) => {
-    const musicManager = Plugins.music;
-    if (!musicManager) return;
-    try {
-        musicManager.queue(client, message);
-    } catch (err) {
-        throw new Error(err);
-    }
+  const musicManager = Plugins.music;
+  if (!musicManager) return;
+  try {
+    musicManager.queue(client, message);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
