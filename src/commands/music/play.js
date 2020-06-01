98d121eb379fa28ae7f27b8005486fe62335b52b
Dan <dan@dancodes.online>
2020-06-01 22:13:25 +0200

    fix disable and queue in src/commands/music/play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,24 +1,25 @@
 module.exports = {
-  aliases: ["p", "play"],
+  aliases: ["pl", "play"],
   use: process.conf.prefix + "play <yt-link>",
   desc: "Play music",
-  // disabled: !(process.conf.music && process.conf.economy.music),
+  disabled: !(process.conf.music && process.conf.music.enabled),
 };
-
+const regex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/;
 module.exports.run = async (client, message, args) => {
   const musicManager = Plugins.music;
   if (!musicManager) return;
-  musicManager
-    .join(client, message, err => {
-      if (err) {
-        throw new Error(err);
-      }
-      console.log(args);
-      try {
-        musicManager.addSong(client, message, args[0]);
-      } catch {
-        musicManager.leave(client, message);
-        throw new Error("Unable to play current song");
-      }
-    })
+  if (!args[0]) throw new Error("You need to provide a Youtube video link");
+  if (!regex.test(args[0]))
+    throw new Error("This is not a valid Youtube video link");
+  musicManager.join(client, message, (err) => {
+    if (err) {
+      return message.channel.send("Whoops: " + err);
+    }
+    try {
+      musicManager.addSong(client, message, args[0]);
+    } catch {
+      musicManager.leave(client, message);
+      return message.channel.send("Whoops: Unable to play current song");
+    }
+  });
 };
