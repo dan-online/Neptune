34e59e5f0d5a7b564e78a5a1dcd207adc908a1d5
Dan <dan@dancodes.online>
2020-06-01 22:13:25 +0200

    fix disable and queue in src/commands/music/pause.js

--- src/commands/music/pause.js
+++ src/commands/music/pause.js
@@ -1,16 +1,16 @@
 module.exports = {
-    aliases: ["pause"],
-    use: process.conf.prefix + "pause",
-    desc: "Pause music",
-    // disabled: !(process.conf.music && process.conf.economy.music),
+  aliases: ["pause"],
+  use: process.conf.prefix + "pause",
+  desc: "Pause music",
+  disabled: !(process.conf.music && process.conf.music.enabled),
 };
 
 module.exports.run = async (client, message, args) => {
-    const musicManager = Plugins.music;
-    if (!musicManager) return;
-    try {
-        musicManager.pause(client, message);
-    } catch (err) {
-        throw new Error(err);
-    }
+  const musicManager = Plugins.music;
+  if (!musicManager) return;
+  try {
+    musicManager.pause(client, message);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
