78fa293abbf5fe230c6106692f3a6da783154e49
Dan <dan@dancodes.online>
2020-06-08 15:10:55 +0200

    fix sentry

--- src/modules/Sentry.js
+++ src/modules/Sentry.js
@@ -1,8 +1,8 @@
-const Sentry = require("@sentry/node");
-
+if (process.env.Sentry) {
+  var Sentry = require("@sentry/node");
+  Sentry.init({ dsn: process.env.SENTRY });
+}
 module.exports = {
-  module: process.env.SENTRY
-    ? Sentry.init({ dsn: process.env.SENTRY })
-    : { captureException: () => false },
+  module: process.env.SENTRY ? Sentry : { captureException: () => false },
   name: "Sentry",
 };
