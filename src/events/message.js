70d7eff1962acf98b6bdba9a42067df27ff98902
Dan <dan@dancodes.online>
2020-06-08 15:06:48 +0200

    sentry be trappin

--- src/events/message.js
+++ src/events/message.js
@@ -66,6 +66,7 @@ module.exports = {
       }
     }
     function catchErr(err) {
+      Sentry.captureError(err);
       if (err.name == "Error") {
         return message.channel.send(
           "Whoops: *" +
