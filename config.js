37557dd776cde6fa1467961b4cceee4ff59e3c32
LightenedLimited <macnguyenanh@gmail.com>
2020-06-10 16:59:30 +0200

    This was a mistake

--- config.js
+++ config.js
@@ -1,99 +1,61 @@
 module.exports = {
-  prefix: "t!",
-  owner: ["312551747027468290", "209300151485988864"], // your discord id
-  color: "#00ffe1",
-  // full: true,
-  emojis: {
-    err: {
-      id: "🔴",
-      full: ":red_circle:",
+    "prefix": "t!",
+    "owner": ["312551747027468290", "209300151485988864"],
+    "color": "#00ffe1",
+    "emojis": {
+        "err": {
+            "id": "🔴",
+            "full": ":red_circle:"
+        },
+        "success": {
+            "id": "✅",
+            "full": ":white_check_mark:"
+        }
     },
-    success: {
-      id: "✅",
-      full: ":white_check_mark:",
+    "persistent": true,
+    "mods": {
+        "enabled": true
     },
-  },
-  persistent: true,
-  // economy: {
-  //   enabled: true,
-  // },
-  mods: {
-    enabled: true,
-  },
-  website: {
-    enabled: true,
-    status: true,
-  },
-  //   port: 8080,
-  //   status: true,
-  // routes: [
-  //   {
-  //     route: "/",
-  //     handler: "./src/webhooks/home.js",
-  //   },
-  //   {
-  //     route: "/announce",
-  //     handler: "./src/webhooks/announce.js",
-  //   },
-  // ],
-  // },
-  // email: {
-  //   enabled: false,
-  //   username: process.env.EMAIL_USERNAME,
-  //   password: process.env.EMAIL_PASSWORD,
-  //   service: "gmail",
-  //   role: {
-  //     data: {
-  //       name: "Verified",
-  //     },
-  //   },
-  // },
-  webhooks: {
-    enabled: true,
-    paths: [{
-      icon: "https://dancodes.online/assets/images/me.png",
-      name: "Test",
-      path: "test",
-      map: `New test from {{name}}`,
-      debug: true,
-      secret: {
-        key: "123", // should be replaced with a .env for production
-      },
-      channel: "596047625736814614",
-      // Post/Get request to /webhooks/test with body/query: { name: "DanCodes", key: "123" }
-    }, ],
-  },
-  // translate: {
-  //   enabled: true,
-  // },
-  // music: {
-  //   enabled: true,
-  // },
-  settings: {
-    enabled: true,
-    autoRole: true,
-  },
-  tickets: {
-    enabled: true,
-  },
-  reactionRoles: {
-    enabled: true,
-  },
-
-  welcome: {
-    enabled: true,
-    channel: "bot-testing-zone",
-    accentColor: "#7289DA",
-    fontPath: path.resolve(
-      __dirname,
-      "src",
-      "assets",
-      "fonts",
-      "Poppins-Regular.ttf"
-    ),
-  },
-  app: {
-    enabled: true,
-    port: 3000
-  }
-};
+    "website": {
+        "enabled": true,
+        "status": true,
+        "port": "8080"
+    },
+    "webhooks": {
+        "enabled": true,
+        "paths": [{
+            "icon": "https://dancodes.online/assets/images/me.png",
+            "name": "Test",
+            "path": "test",
+            "map": "New test from {{name}}",
+            "debug": true,
+            "secret": {
+                "key": "123"
+            },
+            "channel": "596047625736814614"
+        }]
+    },
+    "settings": {
+        "enabled": true,
+        "autoRole": true
+    },
+    "tickets": {
+        "enabled": true
+    },
+    "reactionRoles": {
+        "enabled": true
+    },
+    "welcome": {
+        "enabled": true,
+        "channel": "bot-testing-zone",
+        "accentColor": "#7289DA",
+        "fontPath": "C:\\Users\\Alex\\Desktop\\Projects\\Discord-Bot\\src\\assets\\fonts\\Poppins-Regular.ttf"
+    },
+    "app": {
+        "enabled": true,
+        "port": 3000
+    },
+    "economy": {
+        "enabled": false
+    }
+}
--- src/plugins/app.js
+++ src/plugins/app.js
@@ -1,113 +1,86 @@
 const {
-    commands
+  commands
 } = require("../bot");
+
+const spawn = require("cross-spawn");
+const ss = require("socket.io-stream");
 class App {
-    constructor(config) {
-        this.port = config.port || 8081;
-        console.log("inited");
+  constructor(config) {
+    this.port = config.port || 8081;
+    console.log("inited");
+    this.commands = {
+      restart: this.restart,
+      saveConfig: this.saveConfig,
+      handleErrors: this.handleErrors,
+    };
+    this.socketStreamCommands = {
+      streamConsole: this.streamConsole,
+    };
+    return this;
+  }
+  streamConsole(stream, data) {
+    process.stdout.pipe(stream);
+  }
+  newConnection(socket) {
+    socket.emit("config", require("../../config"));
+    Object.keys(this.commands).forEach(command => {
+      socket.on(command, data => {
+        this.commands[command](data);
+      });
+    });
+    Object.keys(this.socketStreamCommands).forEach(command => {
+      ss(socket).on(command, this.socketStreamCommands[command]);
+    });
+  }
+  restart(data) {
+    setTimeout(function () {
+      process.once("exit", function () {
+        const child = require("child_process").spawn(
+          process.argv.shift(),
+          process.argv, {
+            cwd: process.cwd(),
+            detached: true,
+            stdio: "ignore",
+          }
+        );
+      });
+      process.exit();
+    }, 1000);
+  }
+  saveConfig(config) {
+    console.log("we were in config");
+    const app = this;
+    fs.writeFile(
+      path.resolve(__dirname, "..", "..", "config-back.js"),
+      "module.exports = " + JSON.stringify(require("../../config")),
+      err => {
+        app.handleErrors(err);
+        fs.writeFile(
+          path.resolve(__dirname, "..", "..", "config.js"),
+          "module.exports = " + JSON.stringify(config),
+          function (err) {
+            app.handleErrors(err);
+            app.restart();
+          }
+        );
+      }
+    );
+  }
+  handleErrors(err) {}
+  initEarly(plugins) {
+    this.app = plugins.website.app ? plugins.website.app : require("express")();
+    this.server = require("http").createServer(this.app);
+    this.io = require("socket.io")(this.server, {
+      origins: "*:*",
+    });
+    this.io.use(require("socket.io-encrypt")(process.env.SOCKET_KEY));
 
-        this.commands = {
-            restart: this.restart,
-            enablePlugins: this.enablePlugins,
-        };
-        return this;
+    this.io.on("connection", socket => this.newConnection(socket));
+    if (!plugins.website.app) {
+      this.server.listen(this.port);
     }
-    newConnection(socket) {
-        Object.keys(this.commands).forEach(command => {
-            socket.on(command, data => {
-                this.commands[command](data);
-            });
-        });
-    }
-    restart(data) {
-        console.log("This is pid " + process.pid);
-        setTimeout(function () {
-            process.on("exit", function () {
-                console.log("inisde child process");
-                require("child_process").spawn(process.argv.shift(), process.argv, {
-                    cwd: process.cwd(),
-                    detached: true,
-                    stdio: "inherit",
-                });
-            });
-            process.exit();
-        }, 5000);
-    }
-    enableCommands(commandsPath) {
-        commandsPath.forEach(commandPath => {
-            try {
-                delete require.cache[require.resolve(commandPath)];
-                const command = require(commandPath);
-            } catch {}
-            command.alias.forEach(alias => {
-                if (commands.get(alias)) {
-                    log.warn("overwriting existing alias: " + alias);
-                }
-                commands.set(alias, command);
-            });
-        });
-        log.info(commands.size + " commands fully loaded");
-        return commands.size;
-    }
-    enablePlugins(pluginsRequest) {
-        console.log(pluginsRequest);
-        pluginsRequest.forEach(pluginRequest => {
-            //example plugin request
-            /* 
-                  {
-                      name: "economy",
-                      enabled: true, 
-                      coins: "test"
-                  }
-                  */
-
-            if (Plugins[pluginRequest.name]) {
-                return;
-            }
-            process.conf[pluginsRequest.name] = pluginRequest;
-            const plug = require("./" + pluginRequest.name)
-            Plugins[pluginRequest.name] = new plug(
-                pluginRequest
-            );
-            if (Plugins[pluginRequest.name].hasOwnProperty("initEarly")) {
-                Plugins[pluginRequest.name].initEarly(Plugins);
-            }
-            fs.readdir(
-                path.resolve(__dirname, "..", "commands", pluginRequest.name),
-                (err, files) => {
-                    if (err) {
-                        this.handleErrors(err);
-                    }
-                    let filePath = [];
-                    files.forEach(file => {
-                        filePath.push(
-                            path.resolve(
-                                __dirname,
-                                "..",
-                                "commands",
-                                pluginRequest.name,
-                                file
-                            )
-                        );
-                    });
-                    this.enableCommands(filepath);
-                }
-            );
-        });
-    }
-    handleErrors(err) {}
-    initEarly(plugins) {
-        this.app = plugins.website.app ? plugins.website.app : require("express")();
-        this.server = require("http").createServer(this.app);
-        this.io = require("socket.io")(this.server, {
-            origins: "*:*",
-        });
-        this.io.on("connection", socket => this.newConnection(socket));
-        if (!plugins.website.app) {
-            this.server.listen(this.port);
-        }
-        this.server.listen(this.port);
-    }
+    this.server.listen(this.port);
+  }
 }
 
 module.exports = App;
