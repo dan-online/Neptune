699df59e0c10a059519a5bad7aea5b53578df7e2
DanCodes <dan@dancodes.online>
2020-06-11 08:45:43 +0200

    core commands

--- src/commands/core/ram.js
+++ src/commands/core/ram.js
@@ -3,11 +3,19 @@ module.exports = {
   use: process.conf.prefix + "ram",
   desc: "View memory usage",
 };
-
-module.exports.run = async (client, message) => {
+/**
+ * This command provides the ram usage of the process
+ * @function
+ * @param {Discord.Client} client - The client connection
+ * @param {Discord.Message} message - The message sent by the user
+ * @alias Ram
+ */
+async function ramCommand(client, message) {
   message.channel.send(
     "Ram usage is currently **" +
       Math.round((process.memoryUsage().heapUsed / (1024 * 1024)) * 100) / 100 +
       "MB**"
   );
-};
+}
+
+module.exports.run = ramCommand;
