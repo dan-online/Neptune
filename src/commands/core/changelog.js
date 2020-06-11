ebde06bf0167fde03c8ab592895e7e751dac68dc
DanCodes <dan@dancodes.online>
2020-06-11 12:38:22 +0200

    cache data

--- src/commands/core/changelog.js
+++ src/commands/core/changelog.js
@@ -3,6 +3,7 @@ module.exports = {
   use: process.conf.prefix + "changelog",
   desc: "View the changelog of Neptune",
 };
+var cachedChangelog = {};
 /**
  * This command finds commits on github
  * @function
@@ -14,12 +15,33 @@ async function helpCommand(client, message) {
   const embed = new Discord.MessageEmbed()
     .setColor(process.conf.color)
     .setTitle("Changelog");
-  fetch("https://api.github.com/repos/dan-online/Neptune/commits")
-    .then((res) => res.json())
-    .then((json) => {
-        embed.setDescription(json.slice(0, 10).map(x => `[${x.sha.slice(0, 7)}](${x.html_url}) - ${x.commit.message.split("\n")[0]} - [${x.commit.author.name}](${x.author.html_url})`));
-        return message.channel.send(embed)
-    });
+  if (!cachedChangelog.date || new Date() - cachedChangelog.date >= 60000) {
+    fetch("https://api.github.com/repos/dan-online/Neptune/commits")
+      .then((res) => res.json())
+      .then(handleData)
+      .catch((err) =>
+        message.channel.send(
+          "Something went wrong when fetching from github: " + err.message
+        )
+      );
+    cachedChangelog.date = new Date();
+  } else {
+    handleData(cachedChangelog.data);
+  }
+  function handleData(json) {
+    cachedChangelog.data = json;
+    embed.setDescription(
+      json
+        .slice(0, 10)
+        .map(
+          (x) =>
+            `[${x.sha.slice(0, 7)}](${x.html_url}) - ${
+              x.commit.message.split("\n")[0]
+            } - [${x.commit.author.name}](${x.author.html_url})`
+        )
+    );
+    return message.channel.send(embed);
+  }
 }
 
 module.exports.run = helpCommand;
