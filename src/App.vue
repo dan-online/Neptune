00ec0f9e941c32b7a3343f6f840f8546928325c7
LightenedLimited <macnguyenanh@gmail.com>
2020-06-12 16:20:53 +0200

    Complete rewrite to use library

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-      <router-link to="/config">Config</router-link>
-    </div>
-    <router-view />
+  <div>
+    <notifications></notifications>
+    <router-view :key="$route.fullPath"></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  sockets: {
-    connect: function() {
-      console.log("socket connected");
+  export default {
+    methods: {
+      disableRTL() {
+        if (!this.$rtl.isRTL) {
+          this.$rtl.disableRTL();
+        }
+      },
+      toggleNavOpen() {
+        let root = document.getElementsByTagName('html')[0];
+        root.classList.toggle('nav-open');
+      }
     },
-  },
-};
+    mounted() {
+      this.$watch('$route', this.disableRTL, { immediate: true });
+      this.$watch('$sidebar.showSidebar', this.toggleNavOpen)
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style lang="scss"></style>
