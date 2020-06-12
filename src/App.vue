a139084a64bd53f619e27cc7041c912ed2ed8d64
Dan <dan@dancodes.online>
2020-06-12 11:20:47 +0200

    vue temp

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,6 @@ export default {
     connect: function() {
       console.log("socket connected");
     },
-    customEmit: function(data) {
-      console.log(
-        'this method was fired by the socket server. eg: io.emit("customEmit", data)'
-      );
-    },
-  },
-  mounted() {
-    console.log(this.$socket);
   },
 };
 </script>
